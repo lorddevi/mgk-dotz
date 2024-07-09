42f993b79bbc37f93eba7756f1b00b67ab58ec1d
Lord_Devi <lord@mgk.one>
2024-07-09 17:04:49 -0400

    Broke neovim config into minimal bootstrap.

--- .config/yadm/shell-extras.d/06-neovim.sh
+++ .config/yadm/shell-extras.d/06-neovim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-_location=".config/yadm/shell-minimal.d/07-vim.sh"
+_location=".config/yadm/shell-extras.d/06-neovim.sh"
 _ghq="${HOME}/.local/opt/go/bin/ghq"
 
 # {{{ <mordu debug system>
@@ -50,11 +50,11 @@ _clone_vim_plug() {
 _create_autoload_directories() {
 	# Make sure the autoload directories exist for vim and nvim.
 	_mordu "Creating autoload directories."
-	if [ -d "${HOME}/.config/vim/autoload" ] ; then
-		_mordu "Vim autoload dir already exists."
+	if [ -d "${HOME}/.config/nvim/autoload" ] ; then
+		_mordu "Neovim autoload dir already exists."
 	else
-		_mordu "Creating vim autoload dir."
-		mkdir -p "${HOME}/.config/vim/autoload"
+		_mordu "Creating neovim autoload dir."
+		mkdir -p "${HOME}/.config/nvim/autoload"
 	fi
 	_mordu "Finished creating autoload directories."
 }
@@ -64,33 +64,31 @@ _create_autoload_directories() {
 _link_vim_plug_to_autoloads() {
 	local __opt_plug_vim="${HOME}/.local/opt/git/git.mgk.one/vim/junegunn.vim-plug/plug.vim"
 
-	# Add Vim-Plug to vim's autoload dir if it isn't already.
-	_mordu_n "Checking for vim's vim-plug link.."
-	if [ -f "${HOME}/.config/vim/autoload/plug.vim" ] ; then
+	# Add Vim-Plug to neovim's autoload dir if it isn't already.
+	_mordu_n "Checking for neovim's vim-plug link.."
+	if [ -f "${HOME}/.config/nvim/autoload/plug.vim" ] ; then
 		_mordu_nl "..Found."
 	else
 		_mordu_nl "..Not found.  Linking."
-		ln -sr "$__opt_plug_vim" "${HOME}/.config/vim/autoload/plug.vim"
+		ln -sr "$__opt_plug_vim" "${HOME}/.config/nvim/autoload/plug.vim"
 	fi
 }
 # }}} </link vim-plug to autoloads>
 
-# {{{ <define vim plugins>
-_define_vim_plugins() {
-	_mordu "Defining vim plugin list."
-	_vim_plugins=(\
-		git.mgk.one/vim/bourgeoisbear.clrzr \
-		git.mgk.one/vim/khaveesh.vim-fish-syntax \
-		git.mgk.one/vim/tpope.vim-surround \
-		git.mgk.one/vim/tpope.vim-repeat)
+# {{{ <define neovim plugins>
+_define_neovim_plugins() {
+	_mordu "Defining neovim plugin list."
+	_neovim_plugins=(\
+		git.mgk.one/nvim/norcalli.nvim-colorizer.lua \
+		git.mgk.one/nvim/ishan9299.modus-theme-vim)
 }
-# }}} </define vim plugins>
+# }}} </define neovim plugins>
 
-# {{{ <clone vim plugins>
-_clone_vim_plugins() {
-	_mordu "Cloning vim plugins."
-	for __vim_plugin in "${_vim_plugins[@]}" ; do
-		local __repo="$__vim_plugin"
+# {{{ <clone neovim plugins>
+_clone_neovim_plugins() {
+	_mordu "Cloning neovim plugins."
+	for __neovim_plugin in "${_neovim_plugins[@]}" ; do
+		local __repo="$__neovim_plugin"
 
 		_mordu_n "Checking for ${__repo}.."
 		if [ -d "${HOME}/.local/opt/git/${__repo}" ]; then
@@ -102,12 +100,12 @@ _clone_vim_plugins() {
 		fi
 	done
 }
-# }}} <clone vim plugins>
+# }}} </clone neovim plugins>
 
 # {{{ <running vim plug install>
 _run_vim_plug_install() {
-	_mordu "Calling PlugInstall for vim."
-	vim +PlugInstall +qall
+	_mordu "Calling PlugInstall for nvim."
+	nvim +PlugInstall +qall
 }
 # }}} </running vim plug install>
 
@@ -116,8 +114,8 @@ _main() {
 	_clone_vim_plug
 	_create_autoload_directories
 	_link_vim_plug_to_autoloads
-	_define_vim_plugins
-	_clone_vim_plugins
+	_define_neovim_plugins
+	_clone_neovim_plugins
 	_run_vim_plug_install
 }
 _main
