a55fe80aaec81c7e4273c97e51cf7c99069b6ac9
Lord_Devi <lord@mgk.one>
2024-07-10 18:42:21 -0400

    Working on quicklisp.

--- .config/yadm/workstation-minimal.d/06-install-sbcl-and-quicklisp-stuff.sh
+++ .config/yadm/workstation-minimal.d/06-install-sbcl-and-quicklisp-stuff.sh
@@ -187,11 +187,12 @@ _install_quicklisp_packages() {
 # {{{ <new install quicklisp>
 _new_install_quicklisp() {
 	sbcl --load ~/.local/opt/src/quicklisp.lisp \
-		--eval "(quicklisp-quickstart:install :path \"${HOME}/.local/opt/quicklisp/\")" \
-		--eval '(ql:quickload "clx")' \
-		--eval '(ql:quickload "cl-ppcre")' \
-		--eval '(ql:quickload "alexandria")' \
-		--eval '(quit)'
+		--eval "(quicklisp-quickstart:install :path \"${HOME}/.local/opt/quicklisp/\")"
+
+		#--eval '(ql:quickload "clx")' \
+		#--eval '(ql:quickload "cl-ppcre")' \
+		#--eval '(ql:quickload "alexandria")' \
+		#--eval '(quit)'
 }
 # }}} </new install quicklisp>
 
