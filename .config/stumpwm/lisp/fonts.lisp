6a4cf3d8ba44650cbc8f1c37ebb0045f3373d24a
Lord_Devi <lord@mgk.one>
2024-07-20 17:38:19 -0400

    Working on xorg.

--- .config/stumpwm/lisp/fonts.lisp
+++ .config/stumpwm/lisp/fonts.lisp
@@ -2,6 +2,9 @@
 ;; -*-lisp-*-
 (in-package :stumpwm)
 
+(ql:quickload :clx-truetype)
+(load-module "ttf-fonts")
+
 (defvar *font-size* 16 "Default font size")
 
 (defun get-hostname ()
@@ -21,13 +24,17 @@
             (t 16)))))
 
 (defun set-font-safely ()
-  "Attempt to set the font, with error handling."
+  "Attempt to set the JetBrainsMono NF font, with error handling."
   (handler-case
       (progn
-        (set-font (format nil "-*-fixed-medium-r-normal-*-~D-*-*-*-*-*-iso8859-1" *font-size*))
-        (message "Font set successfully."))
+        (set-font (make-instance 'xft:font 
+                                 :family "JetBrainsMono NF" 
+                                 :subfamily "Regular" 
+                                 :size *font-size* 
+                                 :antialias t))
+        (message "JetBrainsMono NF font set successfully."))
     (error (c)
-      (message "Error setting font: ~A" c)
+      (message "Error setting JetBrainsMono NF font: ~A" c)
       (message "Falling back to default font")
       (ignore-errors
         (set-font "-misc-fixed-medium-r-normal--13-120-75-75-c-70-iso8859-1"))
@@ -39,6 +46,10 @@
         (message "Starting font setup...")
         (set-font-based-on-hostname)
         (message "Hostname-based font size: ~A" *font-size*)
+        (setq clx-truetype::*font-dirs*
+              (append (list (namestring (merge-pathnames ".local/share/fonts" (user-homedir-pathname))))
+                      clx-truetype::*font-dirs*))
+        (xft:cache-fonts)
         (set-font-safely)
         (message "Font setup complete."))
     (error (c)
