3cf1f25bb8f0814021a33a16b0a5d0b4f0338c36
Lord_Devi <lord@mgk.one>
2024-07-21 22:37:56 -0400

    Working on stump.

--- .config/stumpwm/lisp/keymap.lisp
+++ .config/stumpwm/lisp/keymap.lisp
@@ -1,24 +1,172 @@
 ;; vim: set ft=lisp :
 ;; -*-lisp-*-
 
+(fill-keymap *top-map*
+  *escape-key* '*root-map*)
 
-;;;;;;;;;;
-;; Misc
-;;;;;;;;;;
-;; Terminals
-(define-key *root-map* (kbd "c") "exec kitty")
-(define-key *root-map* (kbd "C") "exec alacritty")
-;; Browser (Firefox
-(define-key *root-map* (kbd "b") "exec firefox")
-(define-key *root-map* (kbd "B") "colon1 exec firefox http://")
-;; Ssh somewhere
-(define-key *root-map* (kbd "C-s") "colon1 exec kitty -e ssh ")
+(fill-keymap *root-map*
+  (kbd "c")   "exec xterm"
+  (kbd "C-c") "exec xterm"
+  (kbd "e")   "emacs"
+  (kbd "C-e") "emacs"
+  (kbd "b")   "banish"
+  (kbd "C-b") "banish"
+  (kbd "a")   "time"
+  (kbd "C-a") "time"
+  (kbd "!")   "exec"
+  (kbd "C-g") "abort"
+  *escape-fake-key* "send-escape"
+  (kbd ";")   "colon"
+  (kbd ":")   "eval"
+  (kbd "v")   "version"
+  (kbd "m")   "lastmsg"
+  (kbd "C-m") "lastmsg"
+  (kbd "G")   "vgroups"
+  (kbd "g")   '*groups-map*
+  (kbd "x")   '*exchange-window-map*
+  (kbd "F1")  "gselect 1"
+  (kbd "F2")  "gselect 2"
+  (kbd "F3")  "gselect 3"
+  (kbd "F4")  "gselect 4"
+  (kbd "F5")  "gselect 5"
+  (kbd "F6")  "gselect 6"
+  (kbd "F7")  "gselect 7"
+  (kbd "F8")  "gselect 8"
+  (kbd "F9")  "gselect 9"
+  (kbd "F10") "gselect 10"
+  (kbd "h")   '*help-map*)
 
-;;;;;;;;;;
-;; Emacs
-;;;;;;;;;;
-;; Launch emacsclient if not already loaded.  Otherwise, jump it and
-;; raise it.
-(define-key *root-map* (kbd "e") "decide-on-emacsclient")
-;; Create a brand new emacsclient session if one is already running.
-(define-key *root-map* (kbd "E") "emacsclient-launch")
+(fill-keymap *group-top-map*
+  *escape-key* '*group-root-map*)
+
+(fill-keymap *group-root-map*
+  (kbd "C-u") "next-urgent"
+  (kbd "M-n")     "next"
+  (kbd "M-p")     "prev"
+  (kbd "o")       "other"
+  (kbd "RET")     "expose"
+  (kbd "C-RET")   "expose"
+  (kbd "w")   "windows"
+  (kbd "C-w") "windows"
+  (kbd "DEL") "repack-window-numbers"
+  (kbd "k")   "delete"
+  (kbd "C-k") "delete"
+  (kbd "K")   "kill"
+  (kbd "'")   "select"
+  (kbd "\"")  "windowlist"
+  (kbd "0")   "select-window-by-number 0"
+  (kbd "1")   "select-window-by-number 1"
+  (kbd "2")   "select-window-by-number 2"
+  (kbd "3")   "select-window-by-number 3"
+  (kbd "4")   "select-window-by-number 4"
+  (kbd "5")   "select-window-by-number 5"
+  (kbd "6")   "select-window-by-number 6"
+  (kbd "7")   "select-window-by-number 7"
+  (kbd "8")   "select-window-by-number 8"
+  (kbd "9")   "select-window-by-number 9"
+  (kbd "C-N") "number"
+  (kbd "#")   "mark"
+  (kbd "F11") "fullscreen"
+  (kbd "A")   "title"
+  (kbd "i")   "info"
+  (kbd "I")   "show-window-properties")
+
+(fill-keymap *tile-group-top-map*
+  *escape-key* '*tile-group-root-map*)
+
+(fill-keymap *tile-group-root-map*
+  (kbd "n")       "pull-hidden-next"
+  (kbd "C-n")     "pull-hidden-next"
+  (kbd "C-M-n")   "next-in-frame"
+  (kbd "SPC")     "pull-hidden-next"
+  (kbd "C-SPC")   "pull-hidden-next"
+  (kbd "p")       "pull-hidden-previous"
+  (kbd "C-p")     "pull-hidden-previous"
+  (kbd "C-M-p")   "prev-in-frame"
+  (kbd "P")       "place-current-window"
+  (kbd "W")       "place-existing-windows"
+  *escape-key*     "pull-hidden-other"
+  (kbd "M-t")     "other-in-frame"
+  (kbd "C-0")     "pull 0"
+  (kbd "C-1")     "pull 1"
+  (kbd "C-2")     "pull 2"
+  (kbd "C-3")     "pull 3"
+  (kbd "C-4")     "pull 4"
+  (kbd "C-5")     "pull 5"
+  (kbd "C-6")     "pull 6"
+  (kbd "C-7")     "pull 7"
+  (kbd "C-8")     "pull 8"
+  (kbd "C-9")     "pull 9"
+  (kbd "R")       "remove"
+  (kbd "s")       "vsplit"
+  (kbd "S")       "hsplit"
+  (kbd "r")       "iresize"
+  (kbd "o")       "fnext"
+  (kbd "TAB")     "fnext"
+  (kbd "M-TAB")   "fother"
+  (kbd "f")       "fselect"
+  (kbd "F")       "curframe"
+  (kbd "-")       "fclear"
+  (kbd "Q")       "only"
+  (kbd "X")       "remove-split"
+  (kbd "q")       "quit-confirm"
+  (kbd "Up")      "move-focus up"
+  (kbd "Down")    "move-focus down"
+  (kbd "Left")    "move-focus left"
+  (kbd "Right")   "move-focus right"
+  (kbd "M-Up")    "move-window up"
+  (kbd "M-Down")  "move-window down"
+  (kbd "M-Left")  "move-window left"
+  (kbd "M-Right") "move-window right"
+  (kbd "+")       "balance-frames"
+  (kbd "l")       "redisplay"
+  (kbd "C-l")     "redisplay")
+
+(fill-keymap *groups-map*
+  (kbd "g")     "groups"
+  (kbd "c")     "gnew"
+  (kbd "n")     "gnext"
+  (kbd "C-n")   "gnext"
+  (kbd "SPC")   "gnext"
+  (kbd "C-SPC") "gnext"
+  (kbd "N")     "gnext-with-window"
+  (kbd "p")     "gprev"
+  (kbd "C-p")   "gprev"
+  (kbd "P")     "gprev-with-window"
+  (kbd "o")     "gother"
+  (kbd "'")     "gselect"
+  (kbd "\"")    "grouplist"
+  (kbd "m")     "gmove"
+  (kbd "M")     "gmove-marked"
+  (kbd "k")     "gkill"
+  (kbd "A")     "grename"
+  (kbd "r")     "grename"
+  (kbd "1")     "gselect 1"
+  (kbd "2")     "gselect 2"
+  (kbd "3")     "gselect 3"
+  (kbd "4")     "gselect 4"
+  (kbd "5")     "gselect 5"
+  (kbd "6")     "gselect 6"
+  (kbd "7")     "gselect 7"
+  (kbd "8")     "gselect 8"
+  (kbd "9")     "gselect 9"
+  (kbd "0")     "gselect 10")
+(fill-keymap *exchange-window-map*
+             (kbd "Up")    "exchange-direction up"   
+             (kbd "Down")  "exchange-direction down" 
+             (kbd "Left")  "exchange-direction left" 
+             (kbd "Right") "exchange-direction right"
+             (kbd "p")     "exchange-direction up"   
+             (kbd "n")     "exchange-direction down" 
+             (kbd "b")     "exchange-direction left" 
+             (kbd "f")     "exchange-direction right"
+             (kbd "k")     "exchange-direction up"   
+             (kbd "j")     "exchange-direction down" 
+             (kbd "h")     "exchange-direction left" 
+             (kbd "l")     "exchange-direction right")    
+(fill-keymap *help-map*
+  (kbd "v") "describe-variable"
+  (kbd "f") "describe-function"
+  (kbd "k") "describe-key"
+  (kbd "c") "describe-command"
+  (kbd "w") "where-is")
