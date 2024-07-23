f0c81cb84fbf612dc5261dfaf00ba5f16a6e640e
Lord_Devi <lord@mgk.one>
2024-07-22 23:59:10 -0400

    Working on xorg.

--- .config/stumpwm/lisp/keymap.lisp
+++ .config/stumpwm/lisp/keymap.lisp
@@ -17,7 +17,7 @@
 ;;(define-key *root-map* (kbd "c") "exec kitty")
 ;;(define-key *root-map* (kbd "C") "exec alacritty")
 ;;(define-key *root-map* (kbd "b") "exec firefox")
-(define-key *top-map* (kbd "RET") "exec kitty")
+(define-key *top-map* (kbd "s-RET") "exec kitty")
 (mgk-define-key "c" "exec kitty")
 (mgk-define-key "C" "exec alacritty")
 (mgk-define-key "b" "exec firefox")
